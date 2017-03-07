432d2aa81ae1d2971bd1482c2a8e4fc1db567c17
wilfredkesh <wilfredkesh5@gmail.com>
2017-03-07 16:53:48 -0500

    initial commit after adding pages

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { TabsPage} from "../pages/tabs/tabs";
+import { RecipePage } from "../pages/recipe/recipe";
+import { RecipesPage } from "../pages/recipes/recipes";
+import { EditRecipePage } from "../pages/edit-recipe/edit-recipe";
+import { ShoppingListPage} from "../pages/shopping-list/shopping-list";
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    TabsPage,
+    RecipePage,
+    RecipesPage,
+    EditRecipePage,
+    ShoppingListPage
+
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    TabsPage,
+    RecipePage,
+    RecipesPage,
+    EditRecipePage,
+    ShoppingListPage
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
